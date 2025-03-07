81deef94121038bf90c5ed2e10f49157c2347f23
CristianoFenucci <st10563@iismarconipieralisi.it>
2025-03-07 11:26:39 +0100

    aggiunto codice registrazione

--- app/src/main/java/com/example/miaprimaapplicazione/RegistrationActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/RegistrationActivity.java
@@ -1,6 +1,13 @@
 package com.example.miaprimaapplicazione;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +20,43 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        final User[] insertUser = {null};
+        Button registrationButton = findViewById(R.id.btnSave);
+        Button backButton = findViewById(R.id.btnGoBack);
+
+        EditText registrationName = findViewById(R.id.txtNome);
+        EditText registrationSurname = findViewById(R.id.txtCognome);
+        EditText registrationDate = findViewById(R.id.txtDataNascita);
+        EditText registrationPassword = findViewById(R.id.txtPasswordRegistration);
+        EditText registrationUsername = findViewById(R.id.txtUsernameRegistration);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( RegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nomeInserito = registrationName.getText().toString();
+                String cognomeInserito = registrationSurname.getText().toString();
+                String dataInserita = registrationDate.getText().toString();
+                String passwordInserita = registrationPassword.getText().toString();
+                String usernameInserito = registrationUsername.getText().toString();
+                insertUser[0] = new User(usernameInserito, passwordInserita, nomeInserito, cognomeInserito, dataInserita);
+                //scrittura dei log
+                Log.d("Registrazione", " Nome: " + insertUser[0].getName() + " Cognome: " + insertUser[0].getSurname() + " Data: " + insertUser[0].getDate() + " Password: " + insertUser[0].getPassword() + " Username: " + insertUser[0].getUsername());
+                Toast.makeText(RegistrationActivity.this, "Utente registrato: " + insertUser[0].getUsername(), Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent( RegistrationActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/miaprimaapplicazione/User.java
@@ -0,0 +1,37 @@
+package com.example.miaprimaapplicazione;
+
+public class User {
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+    private String date;
+
+    public User(String username, String password, String name, String surname, String date) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
